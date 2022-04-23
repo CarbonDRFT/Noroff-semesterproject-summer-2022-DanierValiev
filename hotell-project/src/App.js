62a7e76774d1446451458863f83d78b2b3213341
CarbonDRFT <67243227+CarbonDRFT@users.noreply.github.com>
2022-04-23 20:00:45 +0200

    update

--- hotell-project/src/App.js
+++ hotell-project/src/App.js
@@ -1,18 +1,30 @@
-import { BrowserRouter, Routes , Route } from 'react-router-dom';
-import Home from './booking/Home';
-import Login from './auth/Login';
-import Register from './auth/Register';
-import TopNav from './components/TopNav';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./booking/Home";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
+import TopNav from "./components/TopNav";
+import { Link } from "react-router-dom";
 
+const TopNav = () => (
+  <div className="nav bg-light d-flex justify-content-betweens">
+    <Link className="nav-link" to="/"></Link>
+    <Link className="nav-link" to="/login">
+      Login
+    </Link>
+    <Link className="nav-link" to="/register">
+      Register
+    </Link>
+  </div>
+);
 
 function App() {
   return (
     <BrowserRouter>
-    <TopNav />
+      <TopNav />
       <Routes>
-        <Route path="/" element={<Home/>}/> 
-        <Route path="/login" element={<Login/>}/> 
-        <Route path="/register" element={<Register/>}/> 
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
